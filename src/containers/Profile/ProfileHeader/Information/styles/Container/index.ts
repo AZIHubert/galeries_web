f5a205e1ad78a07d23aa5426fb62a8f3ad4728dc
AZIHubert <allan.aoudji@protonmail.com>
2021-03-23 10:48:37 +0100

    update Profile Header style

--- src/containers/Profile/ProfileHeader/Information/styles/Container/index.ts
+++ src/containers/Profile/ProfileHeader/Information/styles/Container/index.ts
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 
+import mediaQueries from '#helpers/mediaQueries';
+
 const Container = styled.div`
   align-items: 'center';
   display: flex;
   flex-direction: column;
   width: 240px;
+  @media ${mediaQueries.laptopL} { 
+    width: 340px;
+  }
 `;
 
 export default Container;
