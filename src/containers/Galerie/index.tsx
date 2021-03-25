eb9697cf2b1a031833a25e594bab9ec86868eb87
AZIHubert <allan.aoudji@protonmail.com>
2021-03-25 15:06:09 +0100

    style galerie header

--- src/containers/Galerie/index.tsx
+++ src/containers/Galerie/index.tsx
@@ -7,6 +7,12 @@ import {
   useHistory,
   useParams,
 } from 'react-router-dom';
+import styled from 'styled-components';
+
+import mediaQueries from '#helpers/mediaQueries';
+
+import Button from '#components/Button';
+import Text from '#components/Text';
 
 import {
   fetchGalerie,
@@ -15,6 +21,71 @@ import {
   galeriesSelector,
   galerieStatusSelector,
 } from '#store/selectors';
+
+const Container = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin: ${({ theme }) => (
+    `0 ${theme.wrapper.margin.smallest}px`
+  )};
+  min-height: 100vh;
+  padding-top: ${({ theme }) => (
+    `${theme.header.dashboard.height.small}px`
+  )};
+  @media ${mediaQueries.mobileL} {
+    margin: ${({ theme }) => (
+    `0 ${theme.wrapper.margin.small}px`
+  )};
+  }
+  @media ${mediaQueries.laptop} {
+    margin: ${({ theme }) => (
+    `0 ${theme.wrapper.margin.medium}px`
+  )};
+    padding-top: ${({ theme }) => (
+    `${theme.header.dashboard.height.medium}px`
+  )};
+  }
+  @media ${mediaQueries.laptopL} { 
+    margin: ${({ theme }) => (
+    `0 ${theme.wrapper.margin.large}px`
+  )};
+    padding-top: ${({ theme }) => (
+    `${theme.header.dashboard.height.large}px`
+  )};
+  }
+`;
+
+interface CoverPictureI {
+  backgroundColor: string;
+}
+
+const CoverPicture = styled.div<CoverPictureI>`
+  background-image: ${({ backgroundColor }) => backgroundColor};
+  width: 75%;
+  height: 320px;
+  border-radius: 0 0 10px 10px;
+`;
+
+const Header = styled.header`
+  width: 100%;
+  display: flex;
+  border-radius: 0 0 10px 10px;
+  align-items: center;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.colors.white};
+  border-left: ${({ theme }) => (`2px solid ${theme.colors.primary}`)};
+  border-right: ${({ theme }) => (`2px solid ${theme.colors.primary}`)};
+  border-bottom: ${({ theme }) => (`2px solid ${theme.colors.primary}`)};
+`;
+
+const HeaderText = styled.div`
+  width: 75%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 40px 0;
+`;
 
 const Galerie = () => {
   const dispatch = useDispatch();
@@ -55,11 +126,40 @@ const Galerie = () => {
   ]);
 
   // If galerie fetch, then error, history.push('/dashboard')
+  if (!galerie) {
+    return null;
+  }
 
   return (
-    <div style={{ marginTop: 100 }}>
-      {galerie ? galerie.name : 'loading'}
-    </div>
+    <Container>
+      <Header>
+        <CoverPicture
+          backgroundColor={galerie.defaultCoverPicture}
+        />
+        <HeaderText>
+          <div>
+            <Text
+              fontWeight='bold'
+              styles={{
+                fontSize: 1.9,
+                lineHeight: 2.3,
+              }}
+            >
+              {galerie.name}
+            </Text>
+            <Text>
+            10 members
+            </Text>
+          </div>
+          <div>
+            <Button.Default
+              variant='secondary'
+              title='create an invitation'
+            />
+          </div>
+        </HeaderText>
+      </Header>
+    </Container>
   );
 };
 
